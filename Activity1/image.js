1b60414aa617c62610d4ff7f7cbf792e64aa6443
DarelBriones13 <darelsabaria123@gmail.com>
2024-04-01 23:08:05 +0800

    commit

--- Activity1/image.js
+++
@@ -1,9 +0,0 @@
-const rightDiv = (image) => {
-    let mydiv = document.createElement("mydiv")
-    mydiv.className = "RightClass"
-    mydiv.innerHTML = `<img src="./image/${image}">`
-
-    return mydiv
-}
-
-export {rightDiv}
--- Activity2/app.js
+++ Activity2/app.js
@@ -2,9 +2,10 @@ import { rightDiv } from "./image.js"
 import { leftdiv } from "./info.js"
 
 const data ={
-    title1:      "Dog",
+    title1:      "Golden Retriever Pup",
     para1:      "Meet Max  a lively golden retriever with a heart of gold and a wagging tail that never stops With his fluffy coat shining in the sunlight he's always ready for an adventure whether it's chasing balls in the park or cuddling up on the couch for a cozy movie night.",
     myImage:    "dog.jpg",
+    
 }
 
 const{ title1,  para1,  myImage,} = data
